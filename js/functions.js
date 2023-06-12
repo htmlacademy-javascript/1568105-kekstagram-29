6990dc0e9958f0743050f2742d4a059f0f1de3c5
aManAndCode <105123221+aManAndCode@users.noreply.github.com>
2023-06-13 01:42:03 +0300

    ДЗ#2 готово

---
+++ js/functions.js
@@ -0,0 +1,35 @@
+// Функция для проверки длины строки
+// function lengthCheck (str, lettersLimit) {
+//   if (str.length < lettersLimit) {
+//     console.log('Cтрока короче 20 символов', true)
+//   } else if (str.length === lettersLimit) {
+//     console.log('Длина строки ровно 18 символов', true)
+//   } else if (str.length >= lettersLimit) {
+//     console.log('Строка длиннее 10 символов', false)
+//   }
+//   console.log(str.length);
+// }
+
+// lengthCheck('проверяемая строка', 10);
+
+
+// Функция для проверки строки на палиндром
+// function isPalindrome(str) {
+//   let editedStr = str.replaceAll(' ', '').toLowerCase();
+//   let newStr = '';
+
+//   for (let i = editedStr.length - 1; i >= 0; i--) {
+//     newStr += editedStr[i];
+//   }
+
+//   if (editedStr === newStr) {
+//     console.log('Палиндром');
+//   } else {
+//     console.log('Не палиндром');
+//   }
+// }
+
+// isPalindrome('Лёша на полке клопа нашёл ')
+
+
+
---
+++ js/tasks.js
@@ -0,0 +1,54 @@
+const users = [
+  {
+    id: 1,
+    name: 'John',
+    isActive: true,
+    age: 19
+  },
+  {
+    id: 2,
+    name: 'Rick',
+    isActive: true,
+    age: 25
+  },
+  {
+    id: 3,
+    name: 'Anthony',
+    isActive: false,
+    age: 15
+  }
+];
+
+const getNames = (arr) => {
+  const names = [];
+  for (let i = 0; i < getNames.length - 1; i++) {
+    if (arr[i].isActive) {
+      names.push(arr[i]);
+    }
+  }
+  return names;
+};
+
+const getNames1 = (arr) => arr.map((user) => user.name );
+
+const getActiveUsers = (arr) => arr.filter((item) => item.isActive).map((activeUser) => activeUser.name)
+
+const sortByAge = (arr) => arr.sort((item1, item2) => item1.age > item2.age ? 1 : -1).map((user) => `${ user.name}: ${ user.age}`)
+
+// console.log(getNames1(users));
+// console.log(getActiveUsers(users));
+console.log(sortByAge(users));
+
+// Функция возвращающая целые положительные числа
+// function returnIntegers(str) {
+//   let newString = '';
+
+//   // for (let i = 0; i < str.length; i++) {
+//   //   newString += parseInt(str[i], 10)
+//   // }
+//   // console.log(newString);
+//   console.log(parseInt(str, 10));
+// }
+
+// returnIntegers('2023 год')
+// returnIntegers('ECMAScript 2022')
