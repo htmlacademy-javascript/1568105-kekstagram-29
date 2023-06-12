73d818ade9a5e89f99c8dfba09a4e95e3b9eea30
aManAndCode <105123221+aManAndCode@users.noreply.github.com>
2023-06-13 01:53:13 +0300

    ДЗ#2 готово

--- js/tasks.js
+++ js/tasks.js
@@ -1,45 +1,45 @@
-const users = [
-  {
-    id: 1,
-    name: 'John',
-    isActive: true,
-    age: 19
-  },
-  {
-    id: 2,
-    name: 'Rick',
-    isActive: true,
-    age: 25
-  },
-  {
-    id: 3,
-    name: 'Anthony',
-    isActive: false,
-    age: 15
-  }
-];
+// const users = [
+//   {
+//     id: 1,
+//     name: 'John',
+//     isActive: true,
+//     age: 19
+//   },
+//   {
+//     id: 2,
+//     name: 'Rick',
+//     isActive: true,
+//     age: 25
+//   },
+//   {
+//     id: 3,
+//     name: 'Anthony',
+//     isActive: false,
+//     age: 15
+//   }
+// ];
 
-const getNames = (arr) => {
-  const names = [];
-  for (let i = 0; i < getNames.length - 1; i++) {
-    if (arr[i].isActive) {
-      names.push(arr[i]);
-    }
-  }
-  return names;
-};
+// const getNames = (arr) => {
+//   const names = [];
+//   for (let i = 0; i < getNames.length - 1; i++) {
+//     if (arr[i].isActive) {
+//       names.push(arr[i]);
+//     }
+//   }
+//   return names;
+// };
 
-const getNames1 = (arr) => arr.map((user) => user.name );
+// const getNames1 = (arr) => arr.map((user) => user.name );
 
-const getActiveUsers = (arr) => arr.filter((item) => item.isActive).map((activeUser) => activeUser.name)
+// const getActiveUsers = (arr) => arr.filter((item) => item.isActive).map((activeUser) => activeUser.name)
 
-const sortByAge = (arr) => arr.sort((item1, item2) => item1.age > item2.age ? 1 : -1).map((user) => `${ user.name}: ${ user.age}`)
+// const sortByAge = (arr) => arr.sort((item1, item2) => item1.age > item2.age ? 1 : -1).map((user) => `${ user.name}: ${ user.age}`)
 
-// console.log(getNames1(users));
-// console.log(getActiveUsers(users));
-console.log(sortByAge(users));
+// // console.log(getNames1(users));
+// // console.log(getActiveUsers(users));
+// console.log(sortByAge(users));
 
-// Функция возвращающая целые положительные числа
+// // Функция возвращающая целые положительные числа
 // function returnIntegers(str) {
 //   let newString = '';
 
